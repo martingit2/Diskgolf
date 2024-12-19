b38ceeae322ab027bb12c31268acf4779c901eee
Martin <martinp@dubium.no>
2024-12-19 17:10:44 +0100

    Forbereder deploy til Vercel

--- components/Map.tsx
+++ components/Map.tsx
@@ -9,7 +9,7 @@ import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
-// @ts-ignore: Ignorerer TypeScript-feil fordi _getIconUrl er en intern egenskap
+// @ts-expect-error Ignorerer TypeScript-feil fordi _getIconUrl er en intern egenskap
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconUrl: markerIcon.src,
