bfcd76c5f893b2b8952ad857b09ad48f0cba01ce
Martin <martinp@dubium.no>
2024-12-29 16:49:27 +0100

    2fa burde funke v3

--- app/actions/settings.ts
+++ app/actions/settings.ts
@@ -30,7 +30,7 @@ export const settings = async (values: z.infer<typeof SettingsSchema>) => {
     values.email = undefined;
     values.password = undefined;
     values.newPassword = undefined;
-    values.isTwoFactorEnabled = undefined;
+    values.isTwoFactorEnable = undefined;
   }
 
   // Håndter e-postoppdatering
@@ -66,9 +66,9 @@ export const settings = async (values: z.infer<typeof SettingsSchema>) => {
   // Kartlegg felter for databasen
   const updatedData = {
     ...values,
-    isTwoFactorEnable: values.isTwoFactorEnabled, // Mapper felt
+    isTwoFactorEnable: values.isTwoFactorEnable, // Mapper felt
   };
-  delete updatedData.isTwoFactorEnabled; // Fjern det som ikke finnes i databasen
+  delete updatedData.isTwoFactorEnable; // Fjern det som ikke finnes i databasen
 
   // Oppdater bruker i databasen
   try {
