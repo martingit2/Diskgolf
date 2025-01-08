71cbf2592dcaa616932e71d6b512ffc9fb97cc9c
martingit2 <157295430+martingit2@users.noreply.github.com>
2025-01-08 13:44:06 +0100

    Update logout.ts

--- app/actions/logout.ts
+++ app/actions/logout.ts
@@ -2,5 +2,5 @@
 
 export const logout = async () => {
   console.log("Utfører server-relaterte handlinger ved utlogging.");
-  // Legg til logikk for eventuelle server-operasjoner ved utlogging
+  // Her kan vi legge til logikk for eventuelle server-operasjoner ved utlogging
 };
