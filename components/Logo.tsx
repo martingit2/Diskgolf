535184d4b5e084ec32447d4ce3be347a432ef982
Martin <martinp@dubium.no>
2025-01-25 21:35:06 +0100

    updata cookiebot for heroku sin configvar

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,7 +3,7 @@
 /* Denne må oppdateres for å fungere alt står nå bare i Header.tsx
  * Vi ønsker å bruke disse pga de er gjenbrukbare komponenter og gjør det mer oversiktlig å lese kode
  * om vi ser i Header.tsx er det veldig langt derfor bruker vi mindre komponenter for å gjøre ting lettere å forstå og utvikle
- * men disse er ikke oppdatert per nå, så de ligger her. Men dette må fikses.
+ * men disse er ikke oppdatert per nå, så de ligger her. Men dette må fikses senere. 
  */
 
 
