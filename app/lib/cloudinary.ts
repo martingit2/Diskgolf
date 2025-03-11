b6fac8958fd234b4fb4b09357b8bd967b44a272a
Martin <martinp@dubium.no>
2025-03-11 14:06:39 +0100

    oppdatert cloudinary.ts med variabel navn

--- app/lib/cloudinary.ts
+++ app/lib/cloudinary.ts
@@ -1,11 +1,10 @@
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-    cloud_name: "dmuhg7btj",
-    api_key: "162139118737331",
-    api_secret: "pZmAaD9ErdzYBM7gsa8vTPIkrvU",
-    secure: true,
-  });
-  
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+});
 
 export default cloudinary;
